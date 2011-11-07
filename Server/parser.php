c252dbcb00ae5c8cb94991259e5eb4af5aa83ceb
Pau Iranzo <paugnu@gmail.com>
2011-11-07 07:38:32 +0100

    parser.php revision (added debug options)

--- Server/parser.php
+++ Server/parser.php
@@ -6,72 +6,66 @@
 require( 'lib/db.php' );
 require( 'lib/class/result.php' );
 
+if ( isset ($_GET['debug']))
+{
+	error_reporting(E_ALL);
+}
+
+
 $result = new Result();
 
-$xmlstring = utf8_decode($_POST['xml']);
-$xmlstring = str_replace ( '\\', '', $xmlstring);
+if ( isset($_POST['xml']))
+{
+	$xmlstring = utf8_decode($_POST['xml']);
+	$xmlstring = str_replace ( '\\', '', $xmlstring);
 
-/* -- XML string example
-$xmlstring =
-<?xml version='1.0'?>
-<execution>
-<application MajorVersion='0' MinorVersion='1' Revision='2' />
-<operating OSMajorVersion='6' OSMinorVersion='0' SPMajorVersion='2' SPMinorVersion='0' SuiteMask='768' Bits='32'/>
+	//Transform the xml string into a simplexml object
+	$xml = simplexml_load_string($xmlstring, 'SimpleXMLElement', LIBXML_NOCDATA | LIBXML_NOBLANKS);
 
-<actions>
-<action id='1' version='' result='0'/>
-<action id='3' version='' result='0'/>
-<action id='4' version='' result='4'/>
-<action id='2' version='' result='0'/>
-</actions>
-</execution>
-;
--- */
+	//If the xml is a file
+	//$xml = simplexml_load_file("xml/model.xml");
 
-//Transform the xml string into a simplexml object
-$xml = simplexml_load_string($xmlstring, 'SimpleXMLElement', LIBXML_NOCDATA | LIBXML_NOBLANKS);
+	if (!empty($xml))
+	{
+	        //Application
+        	$result->MajorVersion = $xml->application['MajorVersion'][0];
+        	$result->MinorVersion = $xml->application['MinorVersion'][0];
+        	$result->Revision = $xml->application['Revision'][0];
 
-//If the xml is a file
-//$xml = simplexml_load_file("xml/model.xml");
+        	//Operating
+        	$result->OSMajorVersion = $xml->operating['OSMajorVersion'][0];
+        	$result->OSMinorVersion = $xml->operating['OSMinorVersion'][0];
+        	$result->SPMajorVersion = $xml->operating['SPMajorVersion'][0];
+        	$result->SPMinorVersion = $xml->operating['OSMajorVersion'][0];
+        	$result->SuiteMask = $xml->operating['SuiteMask'][0];
+       		$result->Bits = $xml->operating['Bits'][0];
 
-if (!empty($xml))
-{              
-        //Application
-        $result->MajorVersion = $xml->application['MajorVersion'][0];
-        $result->MinorVersion = $xml->application['MinorVersion'][0];
-        $result->Revision = $xml->application['Revision'][0];
+		//Get the new SessionID in order to save actions
+        	$SessionID =  $result->save_session();
 
-        //Operating
-        $result->OSMajorVersion = $xml->operating['OSMajorVersion'][0];
-        $result->OSMinorVersion = $xml->operating['OSMinorVersion'][0];
-        $result->SPMajorVersion = $xml->operating['SPMajorVersion'][0];
-        $result->SPMinorVersion = $xml->operating['OSMajorVersion'][0];
-        $result->SuiteMask = $xml->operating['SuiteMask'][0];
-        $result->Bits = $xml->operating['Bits'][0];
-        
-        //Get the new SessionID in order to save actions
-        $SessionID =  $result->save_session();
-
-        if ( $xml->actions->action )
-        {
-                echo '<div style="margin: 0 auto; width: 400px; padding: 1px 20px 20px; background: #CCC; font-family: Arial; font-size: 13px;"><h2>Info</h2>';
-                foreach ( $xml->actions->action as $action )
-                {
-                        $result->add_action($SessionID, $action['id'], $action['version'], $action['result']);
-                        echo 'S\'ha afegit l\'acció: '.$action['id'].' de la sessió '.$SessionID.'<br/>';
-                }
-                echo '</div>';
-        }
-
-        return true;
+        	if ( $xml->actions->action )
+        	{
+                	foreach ( $xml->actions->action as $action )
+                	{
+                        	$result->add_action($SessionID, $action['id'], $action['version'], $action['result']);
+                	}
+        	}
+        	return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 else
 {
-        echo '<div style="margin: 0 auto; width: 400px; padding: 1px 20px 20px; background: #CCC; font-family: Arial; font-size: 13px;">';
-        echo "<h2>Error</h2>No s'ha trobat cap xml o bé l'xml no està ben formatat. <br/><br/>Recordeu enviar-lo per la variable _POST['xml']";
-        echo "</div>";
-        return false;
+	if ( isset ($_GET['debug']))
+	{
+		echo 'No s\'ha rebut cap variable. A continuació es fa un dump de la variable $_POST:';
+		echo '<pre>';var_dump($_POST);echo '</pre>';
+	}
+	else
+		return false;
 }
-
 
 ?>
