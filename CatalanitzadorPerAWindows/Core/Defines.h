074c660a526a30d21005cc6daf9621fe0e7d9b3e
Jordi Mas <jmas@softcatala.org>
2012-01-03 22:17:15 +0100

    Additional files

---
+++ CatalanitzadorPerAWindows/Core/Defines.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#define MAX_LOADSTRING 1024
+
+#ifdef CATALANITZADOR_CREATE_SHARED_LIBRARY
+	#define _APICALL __declspec(dllexport) 	
+#else // Using shared lib
+   #define _APICALL  __declspec(dllimport)    
+#endif 
+
