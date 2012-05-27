759b35f6936145a6e953df0585f7c342c1e15628
Pau Iranzo <paugnu@gmail.com>
2012-05-27 08:10:07 +0200

    Key renamed to KeyVersion for inspectors table. KEY is a reserved name for mysql.

--- Server/mysql/mysqlupgrade-1.0-1.2.sql
+++ Server/mysql/mysqlupgrade-1.0-1.2.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `inspectors` (
   `SessionID` int(11) NOT NULL,
   `InspectorID` int(11) NOT NULL,
-  `Key` char(128) NOT NULL,
+  `KeyVersion` char(128) NOT NULL,
   `Value` char(128) NOT NULL,
   KEY `InspectorID` (`InspectorID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
