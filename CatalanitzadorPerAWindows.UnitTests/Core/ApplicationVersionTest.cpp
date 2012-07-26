db8d8dfe2cf81d8543faf47d8ac67cfdc626ccad
Jordi Mas <jmas@softcatala.org>
2012-07-26 23:13:59 +0200

    Application version class

---
+++ CatalanitzadorPerAWindows.UnitTests/Core/ApplicationVersionTest.cpp
@@ -0,0 +1,112 @@
+﻿/* 
+ * Copyright (C) 2012 Jordi Mas i Hernàndez <jmas@softcatala.org>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+#include "stdafx.h"
+#include "Defines.h"
+#include "ApplicationVersion.h"
+
+using ::testing::StrCaseEq;
+using ::testing::StrCaseNe;
+
+
+TEST(VersionTest, GetComponents_Three)
+{
+	ApplicationVersion version (L"1.2.3");
+	vector <wstring> components;
+	
+	components = version.GetComponents();
+
+	EXPECT_THAT(components.size(), 3);
+	EXPECT_THAT(components[0], StrCaseEq(L"1"));
+	EXPECT_THAT(components[1], StrCaseEq(L"2"));
+	EXPECT_THAT(components[2], StrCaseEq(L"3"));
+}
+
+TEST(VersionTest, GetComponents_Two)
+{
+	ApplicationVersion version (L"1.2");
+	vector <wstring> components;
+	
+	components = version.GetComponents();
+
+	EXPECT_THAT(components.size(), 2);
+	EXPECT_THAT(components[0], StrCaseEq(L"1"));
+	EXPECT_THAT(components[1], StrCaseEq(L"2"));	
+}
+
+TEST(VersionTest, GetComponents_UnBreakable)
+{
+	ApplicationVersion version (L"123");
+	vector <wstring> components;
+	
+	components = version.GetComponents();
+
+	EXPECT_THAT(components.size(), 1);
+	EXPECT_THAT(components[0], StrCaseEq(L"123"));
+}
+
+TEST(VersionTest, GetComponents_Equal)
+{
+	ApplicationVersion versionA (L"1.2.3");
+	ApplicationVersion versionB (L"1.2.3");
+	ApplicationVersion versionC (L"1.2.1");
+
+	EXPECT_TRUE(versionA == versionB);
+	EXPECT_FALSE(versionA == versionC);
+}
+
+TEST(VersionTest, GetComponents_NotEqual)
+{
+	ApplicationVersion versionA (L"1.2.3");
+	ApplicationVersion versionB (L"1.2.3");
+	ApplicationVersion versionC (L"1.2.1");
+
+	EXPECT_FALSE(versionA != versionB);
+	EXPECT_TRUE(versionA != versionC);
+}
+
+TEST(VersionTest, GetComponents_Greater)
+{
+	ApplicationVersion versionA (L"1.2.0");
+	ApplicationVersion versionB (L"1.2.1");
+	ApplicationVersion versionC (L"1.2.2");
+	ApplicationVersion versionD (L"1.1.0");
+	ApplicationVersion versionE (L"0.9.0");
+
+	EXPECT_TRUE(versionB > versionA);
+	EXPECT_TRUE(versionC > versionB);
+	EXPECT_TRUE(versionB > versionD);
+	EXPECT_TRUE(versionC > versionE);
+	EXPECT_FALSE(versionE > versionA);
+}
+
+TEST(VersionTest, GetComponents_Less)
+{
+	ApplicationVersion versionA (L"1.2.0");
+	ApplicationVersion versionB (L"1.2.1");
+	ApplicationVersion versionC (L"1.2.2");
+	ApplicationVersion versionD (L"1.1.0");
+	ApplicationVersion versionE (L"0.9.0");
+
+	EXPECT_TRUE(versionA < versionB);
+	EXPECT_TRUE(versionB < versionC);
+	EXPECT_TRUE(versionD < versionB);
+	EXPECT_TRUE(versionE < versionC);
+	EXPECT_FALSE(versionA < versionE);
+}
---
+++ CatalanitzadorPerAWindows/Core/ApplicationVersion.cpp
@@ -0,0 +1,95 @@
+﻿/* 
+ * Copyright (C) 2012 Jordi Mas i Hernàndez <jmas@softcatala.org>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+#include "stdafx.h"
+#include "ApplicationVersion.h"
+
+#define SEPARATOR L"."
+#define SEPARATOR_LEN 1
+
+vector <wstring> ApplicationVersion::GetComponents(wstring version)
+{
+	vector <wstring> components;
+	int start = 0, end = 0, pos;	
+
+	do
+	{
+		pos = version.find_first_of(SEPARATOR, end);
+		start = end;
+		end = pos;		
+		components.push_back(version.substr(start, end - start));
+		end = end + SEPARATOR_LEN;
+		
+	} while (pos != string::npos);
+
+	return components;
+}
+
+vector <wstring> ApplicationVersion::GetComponents()
+{
+	if (m_components.size() == 0)
+		m_components = GetComponents(m_version);
+
+	return m_components;
+}
+
+bool ApplicationVersion::operator== (ApplicationVersion other)
+{	
+	if (GetComponents().size() != other.GetComponents().size())
+		return false;
+
+	for (unsigned int i = 0; i < GetComponents().size(); i++)
+	{
+		if (GetComponents().at(i) != other.GetComponents().at(i))
+			return false;
+	}
+
+	return true;
+}
+
+bool ApplicationVersion::operator != (ApplicationVersion other)
+{
+	return !(*this == other);
+}
+
+bool ApplicationVersion::operator > (ApplicationVersion other)
+{	
+	if (GetComponents().size() != other.GetComponents().size())
+	{
+		assert(false);
+		return false;
+	}
+
+	for (unsigned int i = 0; i < GetComponents().size(); i++)
+	{
+		wstring a= GetComponents().at(i);
+		wstring b= other.GetComponents().at(i);
+
+		if (GetComponents().at(i) == other.GetComponents().at(i))
+			continue;
+		
+		return GetComponents().at(i) > other.GetComponents().at(i);
+	}
+	return false;
+}
+
+bool ApplicationVersion::operator < (ApplicationVersion other)
+{
+	return !(*this > other);
+}
