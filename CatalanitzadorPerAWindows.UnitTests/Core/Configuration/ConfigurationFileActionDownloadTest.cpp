d1b79e66f9711b684459c98370e8b2c589bee24a
Jordi Mas <jmas@softcatala.org>
2012-08-10 18:38:47 +0200

    ConfigurationFileActionDownload unit test

---
+++ CatalanitzadorPerAWindows.UnitTests/Core/Configuration/ConfigurationFileActionDownloadTest.cpp
@@ -0,0 +1,59 @@
+﻿/* 
+ * Copyright (C) 2012 Jordi Mas i Hernàndez <jmas@softcatala.org>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+#include "stdafx.h"
+#include "Defines.h"
+#include "ConfigurationFileActionDownload.h"
+
+using ::testing::StrCaseEq;
+
+#define MIN_VERSION L"1.2.0"
+#define MAX_VERSION L"1.2.1"
+
+TEST(ConfigurationLatestTest, GetSetMinVersion)
+{	
+	ConfigurationFileActionDownload fileActionDownload;
+	fileActionDownload.SetMinVersion(ApplicationVersion(MIN_VERSION));
+
+	EXPECT_TRUE(fileActionDownload.GetMinVersion() == ApplicationVersion(MIN_VERSION));
+}
+
+TEST(ConfigurationLatestTest, GetSetMaxVersion)
+{	
+	ConfigurationFileActionDownload fileActionDownload;
+	fileActionDownload.SetMaxVersion(ApplicationVersion(MAX_VERSION));
+
+	EXPECT_TRUE(fileActionDownload.GetMaxVersion() == ApplicationVersion(MAX_VERSION));
+}
+
+
+#define LATEST_URL1 L"http://url1.com"
+#define LATEST_URL2 L"http://url1.com"
+
+TEST(ConfigurationLatestTest, GetSetURL)
+{	
+	ConfigurationFileActionDownload fileActionDownload;
+	fileActionDownload.AddUrl(LATEST_URL1);
+	fileActionDownload.AddUrl(LATEST_URL2);
+	
+	EXPECT_THAT(fileActionDownload.GetUrls().size(), 2);
+	EXPECT_THAT(fileActionDownload.GetUrls()[0], StrCaseEq(LATEST_URL1));
+	EXPECT_THAT(fileActionDownload.GetUrls()[1], StrCaseEq(LATEST_URL2));
+}
+
