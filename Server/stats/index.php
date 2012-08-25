c6717deba07a1be9a842cf9df5d9480dd05ce6d3
Xavi Ivars <xavi.ivars@gmail.com>
2012-08-25 16:50:44 +0200

    Fix 2 double axis for session stats

--- Server/stats/index.php
+++ Server/stats/index.php
@@ -245,8 +245,8 @@ $inspectors = array ( 1 => "LibreOffice", 2 => "Skype", 3 => "PDFCreator", 4=> "
 						
 						echo "[Date.UTC($dt[0],",($dt[1]-1),",$dt[2]),",$result->total,"]";
 					} 
-				?>,
-				yAxis:2 ]
+				?>],
+				yAxis:2				
 			}]
 		});
 	});
