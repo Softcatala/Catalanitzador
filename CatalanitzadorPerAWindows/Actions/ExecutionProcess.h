7fcfd554c0898512a2e691dc82ba8c56a050f99b
Jordi Mas <jmas@softcatala.org>
2012-10-07 15:39:18 +0200

    ExecutionProcess class

---
+++ CatalanitzadorPerAWindows/Actions/ExecutionProcess.h
@@ -0,0 +1,52 @@
+/* 
+ * Copyright (C) 2012 Jordi Mas i Hernàndez <jmas@softcatala.org>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+class ExecutionProcess
+{
+	public:
+
+		ExecutionProcess() {}
+
+		ExecutionProcess(wstring name, wstring printableName, bool canClose)
+		{
+			m_name = name;
+			m_printableName = printableName;
+			m_canClose = canClose;
+		}
+
+		wstring GetPrintableName() { return m_printableName;}
+		wstring GetName() { return m_name;}
+		bool CanClose() { return m_canClose;} 
+
+		bool IsEmpty()
+		{
+			return m_name.empty() && m_printableName.empty();
+		}
+
+	private:
+		wstring m_name;
+		wstring m_printableName;
+		bool m_canClose;
+};
