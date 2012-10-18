030035fee0dc67afb831355d71359b8deef2c7dc
Jordi Mas <jmas@softcatala.org>
2012-10-18 07:45:11 +0200

    Bump version to 1.2.8

--- CatalanitzadorPerAWindows/Application/Version.h
+++ CatalanitzadorPerAWindows/Application/Version.h
@@ -20,6 +20,6 @@
 
 #define APP_MAJOR_VERSION		1
 #define APP_MINOR_VERSION		2
-#define APP_REVISION			7
-#define STRING_VERSION			"1.2.7.0"
+#define APP_REVISION			8
+#define STRING_VERSION			"1.2.8.0"
 
