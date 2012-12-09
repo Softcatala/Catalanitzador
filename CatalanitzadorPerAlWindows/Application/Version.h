071ed79be909a65e46ba3b0c6152be3fa28980de
Jordi Mas <jmas@softcatala.org>
2012-12-09 11:23:19 +0100

    Bump version to 1.4.2

--- CatalanitzadorPerAlWindows/Application/Version.h
+++ CatalanitzadorPerAlWindows/Application/Version.h
@@ -20,8 +20,8 @@
 
 #define APP_MAJOR_VERSION		1
 #define APP_MINOR_VERSION		4
-#define APP_REVISION			1
+#define APP_REVISION			2
 
-#define STRING_VERSION				L"1.4.1"
-#define STRING_VERSION_RESOURCES	"1.4.1.0"
+#define STRING_VERSION				L"1.4.2"
+#define STRING_VERSION_RESOURCES	"1.4.2.0"
 
