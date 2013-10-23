70c0cf8479be0a4368a59459b780034692109c9b
Jordi Mas <jmas@softcatala.org>
2013-10-24 00:46:30 +0200

    Bump to dev version 2.2.1

--- CommonCode/Version.h
+++ CommonCode/Version.h
@@ -23,10 +23,10 @@
 
 #define APP_MAJOR_VERSION		2
 #define APP_MINOR_VERSION		2
-#define APP_REVISION			0
+#define APP_REVISION			1
 
-#define STRING_VERSION				_STR("2.2.0")
-#define STRING_VERSION_RESOURCES	"2.2.0.0"
+#define STRING_VERSION				_STR("2.2.1")
+#define STRING_VERSION_RESOURCES	"2.2.1.0"
 
-//#define DEVELOPMENT_VERSION 1 // Comment for release version
+#define DEVELOPMENT_VERSION 1 // Comment for release version
 
