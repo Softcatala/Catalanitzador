e13f333e4bd45b55f473e46593c69afd8e247052
Xavi Ivars <xavi.ivars@gmail.com>
2014-09-25 20:26:01 +0200

    [stats] Show new option added in 2.4.1

--- Server/stats/class.options.php
+++ Server/stats/class.options.php
@@ -6,41 +6,46 @@ class StatsOptions {
     private $_db;
     private $_catalanitzador;
     
-    public function __construct($db,$catalanitzador)
+    public function __construct($db, $catalanitzador)
     {
-        $this->_optionNames = array 
-            ( 0 => "OptionSystemRestore", 1 => "OptionDialect", 2 => "OptionShowSecDlg",
-		3 => "OptionShareTwitter", 4 => "OptionShareFacebook", 5 => "OptionShareGooglePlus");
+        $this->_optionNames = array (
+            0 => "OptionSystemRestore", 
+            1 => "OptionDialect", 
+            2 => "OptionShowSecDlg",
+            3 => "OptionShareTwitter", 
+            4 => "OptionShareFacebook", 
+            5 => "OptionShareGooglePlus", 
+            6 => "OptionSilentInstall"
+        );
         
         $this->_db = $db;
         $this->_catalanitzador = $catalanitzador;
     }
     
-    
     public function GetOptions() {
         
         $v = $this->_catalanitzador->get_version_selected();
-	$p = $this->_catalanitzador->get_platform_selected();  
+        $p = $this->_catalanitzador->get_platform_selected();  
 
-	if(!empty($v) && !empty($p)) {
-		$query = "select distinct OptionId from options, sessions, operatings ".
-                    "where options.SessionID = sessions.Id AND operatings.ID = sessions.OperatingsID ".
-		    "AND ApplicationsID in ($v) and operatings.System = '$p'";
-	} else if(!empty($v)) {
-            $query = "select distinct OptionId from options, sessions ".
-                    "where options.SessionId = sessions.Id AND ApplicationsID in ($v)";
-        } else if(!empty($p)) {
+        if(!empty($v) && !empty($p)) {
+            $query = "select distinct OptionId from options, sessions, operatings ".
+                        "where options.SessionID = sessions.Id AND operatings.ID = sessions.OperatingsID ".
+                "AND ApplicationsID in ($v) and operatings.System = '$p'";
+        } else if(!empty($v)) {
+                $query = "select distinct OptionId from options, sessions ".
+                        "where options.SessionId = sessions.Id AND ApplicationsID in ($v)";
+            } else if(!empty($p)) {
 
-	} else {
-            $query = "select distinct OptionId from options";
+        } else {
+                $query = "select distinct OptionId from options";
         }
         
         $results = $this->_db->get_results($query);
         
         $options = array();
         
-        if(empty($results)) return $options;
-        
+        if (empty($results)) { return $options; }
+
         foreach($results as $result) {
             
             if(isset($this->_optionNames[$result->OptionId])) {
@@ -58,33 +63,31 @@ class StatsOptions {
     public function GetOptionValues($id) {
         $optionValues = array();
         
-        if(!isset($id) || empty($id)) return $optionValues;
+        if(!isset($id) || empty($id)) { return $optionValues; }
         
         $v = $this->_catalanitzador->get_version_selected();
-	$p = $this->_catalanitzador->get_platform_selected();  
+        $p = $this->_catalanitzador->get_platform_selected();  
         
         if(!empty($v)) {
             $v = " AND ApplicationsID in ($v)";
-        }
-        else {
+        } else {
             $v = '';
         }
 
-	if(!empty($p)) {
+        if(!empty($p)) {
             $p = " and operatings.System = '$p'";
-        }
-        else {
+        } else {
             $p = '';
         }
         
-	$query = "select OptionId, Value, count(Value) as ".
+        $query = "select OptionId, Value, count(Value) as ".
                 "Total from options, sessions, operatings where OptionId = $id  ".
                 "AND options.SessionId = sessions.Id AND operatings.ID = sessions.OperatingsID".
-		"$v $p group by OptionId,Value;";
+        "$v $p group by OptionId,Value;";
         
         $results = $this->_db->get_results($query);
         
-        if(empty($results)) return $optionValues;
+        if(empty($results)) { return $optionValues; }
         
         foreach($results as $result) {
             $optionValues[$result->Value] = $result->Total;
