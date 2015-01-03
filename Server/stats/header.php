14f3806b8ecb5109e63165501604b27cf24d3964
Xavi Ivars <xavi.ivars@gmail.com>
2015-01-03 12:43:52 +0100

    [stats] Adding base domain to stats

--- Server/stats/header.php
+++ Server/stats/header.php
@@ -12,9 +12,9 @@
                 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
                 <link rel="stylesheet" href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css" />
                 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
-                <script type="text/javascript" src="/js/highcharts.js"></script>
-                <script type="text/javascript" src="/js/modules/exporting.js"></script>
-                <script type="text/javascript" src="/js/skinny.js"></script>
+                <script type="text/javascript" src="<?=$globals['base_domain']?>/js/highcharts.js"></script>
+                <script type="text/javascript" src="<?=$globals['base_domain']?>/js/modules/exporting.js"></script>
+                <script type="text/javascript" src="<?=$globals['base_domain']?>/js/skinny.js"></script>
                 <script>
                 jQuery(document).ready(function(){
                         jQuery('div').filter(function() {
