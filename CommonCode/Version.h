d468ad5efebb51e438d39fbe7067f4c59534e8d1
Jordi Mas <jmas@softcatala.org>
2016-02-21 10:42:30 +0100

    Bump to version 2.6.6

--- CommonCode/Version.h
+++ CommonCode/Version.h
@@ -23,12 +23,12 @@
 
 #define APP_MAJOR_VERSION		2
 #define APP_MINOR_VERSION		6
-#define APP_REVISION			5
+#define APP_REVISION			6
 
-#define STRING_VERSION				_STR("2.6.5")
-#define STRING_VERSION_RESOURCES	"2.6.5.0"
+#define STRING_VERSION				_STR("2.6.6")
+#define STRING_VERSION_RESOURCES	"2.6.6.0"
 
-#define DEVELOPMENT_VERSION 1 // Comment for release version
+//#define DEVELOPMENT_VERSION 1 // Comment for release version
 
 #if (APP_REVISION % 2 == 0) 
 	#ifdef DEVELOPMENT_VERSION 
