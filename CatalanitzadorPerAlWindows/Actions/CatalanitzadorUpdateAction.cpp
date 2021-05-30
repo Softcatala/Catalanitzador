80bb1eb11f038e6086eaaee01a6ee69408a86976
Jordi Mas <jmas@softcatala.org>
2021-05-30 08:54:10 +0200

    Fix log

--- CatalanitzadorPerAlWindows/Actions/CatalanitzadorUpdateAction.cpp
+++ CatalanitzadorPerAlWindows/Actions/CatalanitzadorUpdateAction.cpp
@@ -91,7 +91,7 @@ void CatalanitzadorUpdateAction::Execute()
 		}
 	}
 
-	g_log.Log(L"CatalanitzadorUpdateAction::Execute ei '%s'", (wchar_t *)application.c_str());
+	g_log.Log(L"CatalanitzadorUpdateAction::Execute '%s'", (wchar_t *)application.c_str());
 	m_runner->Execute(NULL, (wchar_t *)application.c_str(), false);
 }
 
