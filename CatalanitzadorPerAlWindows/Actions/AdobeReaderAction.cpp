c3bec249cc842f3b4e331ed44a974ceff61940af
Jordi Mas <jmas@softcatala.org>
2023-12-09 08:27:37 +0100

    Fix

--- CatalanitzadorPerAlWindows/Actions/AdobeReaderAction.cpp
+++ CatalanitzadorPerAlWindows/Actions/AdobeReaderAction.cpp
@@ -1,5 +1,5 @@
 ﻿/* 
- * Copyright (C) 2012 Jordi Mas i Hernàndez <jmas@softcatala.org>
+ * Copyright (C) 2012-2023 Jordi Mas i Hernàndez <jmas@softcatala.org>
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
